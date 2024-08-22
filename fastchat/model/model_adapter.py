fb42a123e516f7f2e142943f353793673d33cc48
Akira Sasaki <akisarasika@gmail.com>
2024-08-22 10:44:21 +0000

    Reflects changes regarding Llama3 from the latest commit of lm-sys/FastChat (https://github.com/lm-sys/FastChat/tree/29fc8a065a7764bf3c38dd4e399e297496fb2671)

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -1617,6 +1617,22 @@ class Llama2Adapter(BaseModelAdapter):
         return get_conv_template("llama-2")
 
 
+class Llama3Adapter(BaseModelAdapter):
+    """The model adapter for Llama-3 (e.g., meta-llama/Meta-Llama-3-8B-Instruct)"""
+
+    def match(self, model_path: str):
+        return "llama-3" in model_path.lower()
+
+    def load_model(self, model_path: str, from_pretrained_kwargs: dict):
+        model, tokenizer = super().load_model(model_path, from_pretrained_kwargs)
+        model.config.eos_token_id = tokenizer.eos_token_id
+        model.config.pad_token_id = tokenizer.pad_token_id
+        return model, tokenizer
+
+    def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("llama-3")
+
+
 class JLlama2Adapter(BaseModelAdapter):
     """
     Model adapter for llama-2-based models developed internally in SAIJ
@@ -2543,6 +2559,7 @@ register_model_adapter(SolarAdapter)
 register_model_adapter(SteerLMAdapter)
 register_model_adapter(LlavaAdapter)
 register_model_adapter(YuanAdapter)
+register_model_adapter(Llama3Adapter)
 
 # After all adapters, try the default base adapter.
 register_model_adapter(BaseModelAdapter)
